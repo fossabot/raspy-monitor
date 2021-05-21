986bc1732388eba65f819a67bf64fe9cb7e106f6
Syahrial Agni Prasetya <lemniskett@protonmail.com>
2021-05-21 17:46:57 +0700

    Add error handlings.

--- modules/PiStats.py
+++ modules/PiStats.py
@@ -2,6 +2,7 @@ from os import uname, listdir, environ
 from datetime import datetime
 from re import search
 from psutil import virtual_memory, swap_memory, boot_time, getloadavg, disk_usage, cpu_count
+from sys import stderr
 
 
 if 'ROOT_PATH' in environ:
@@ -11,32 +12,40 @@ else:
 
 
 def system():
-    cpu_info_file = open(root_path + 'proc/cpuinfo')
-    cpu_info_lines = cpu_info_file.readlines()
-    cpu_info_file.close()
-    for line in cpu_info_lines:
-        if search('Hardware', line):
-            cpu_name = line.replace('Hardware\t: ', '').replace('\n', '')
-        elif search('Model', line):
-            model = line.replace('Model\t\t: ', '').replace('\n', '')
-    os_release_file = open(root_path + 'etc/os-release')
-    for line in os_release_file.readlines():
-        if search('PRETTY_NAME', line):
-            os_name = line.replace('"', '').replace('PRETTY_NAME=', '').replace('\n', '')
-            break
-    uname_data = uname()
-    return {
-        'model' : model,
-        'processor': cpu_name,
-        'os_name': os_name,
-        'uname': {
-            'os': uname_data.sysname,
-            'arch': uname_data.machine,
-            'hostname': uname_data.nodename,
-            'kernel_ver': uname_data.release,
-            'kernel_build': uname_data.version
+    try:
+        cpu_info_file = open(root_path + 'proc/cpuinfo')
+        cpu_info_lines = cpu_info_file.readlines()
+        cpu_info_file.close()
+
+        for line in cpu_info_lines:
+            if search('Hardware', line):
+                cpu_name = line.replace('Hardware\t: ', '').replace('\n', '')
+            elif search('Model', line):
+                model = line.replace('Model\t\t: ', '').replace('\n', '')
+        os_release_file = open(root_path + 'etc/os-release')
+        for line in os_release_file.readlines():
+            if search('PRETTY_NAME', line):
+                os_name = line.replace('"', '').replace('PRETTY_NAME=', '').replace('\n', '')
+                break
+        uname_data = uname()
+        return {
+            'model' : model,
+            'processor': cpu_name,
+            'os_name': os_name,
+            'uname': {
+                'os': uname_data.sysname,
+                'arch': uname_data.machine,
+                'hostname': uname_data.nodename,
+                'kernel_ver': uname_data.release,
+                'kernel_build': uname_data.version
+            }
         }
-    }
+    except NameError as e:
+        print("Some data can't be parsed, currently only supports parsing data from Raspberry Pi.", file=stderr)
+        print(str(e))
+    except FileNotFoundError as e:
+        print('Error when trying to access resources, is the $ROOT_PATH correct?', file=stderr)
+        print(str(e))
 
 
 def time():
@@ -71,42 +80,50 @@ def memory():
 
 
 def cpu():
-    cpu_num = cpu_count()
-    load_avg = getloadavg()
-    temp_file = open('/sys/devices/virtual/thermal/thermal_zone0/temp')
-    temp = int(int(temp_file.read()) / 1000)
-    temp_file.close()
-    return {
-        'temp': temp,
-        'load': {
-            'last_minute': round(load_avg[0] * 100 / cpu_num, 2),
-            'last_five_minutes': round(load_avg[1] * 100 / cpu_num, 2),
-            'last_fifteen_minutes': round(load_avg[2] * 100 / cpu_num, 2)
+    try:
+        cpu_num = cpu_count()
+        load_avg = getloadavg()
+        temp_file = open(root_path + 'sys/devices/virtual/thermal/thermal_zone0/temp')
+        temp = int(int(temp_file.read()) / 1000)
+        temp_file.close()
+        return {
+            'temp': temp,
+            'load': {
+                'last_minute': round(load_avg[0] * 100 / cpu_num, 2),
+                'last_five_minutes': round(load_avg[1] * 100 / cpu_num, 2),
+                'last_fifteen_minutes': round(load_avg[2] * 100 / cpu_num, 2)
+            }
         }
-    }
+    except FileNotFoundError as e:
+        print('Error when trying to access resources, is the $ROOT_PATH correct?', file=stderr)
+        print(str(e))
 
 
 def netUsage():
-    net_list = []
-    sent_total = 0
-    received_total = 0
-    net_interfaces = listdir(root_path + 'sys/class/net')
-    if 'IGNORE_INTERFACES' in environ:
-        ignored_interfaces = environ['IGNORE_INTERFACES'].split()
-    else:
-        ignored_interfaces = ['lo']
-    for i in net_interfaces:
-        if i not in ignored_interfaces:
-            s_file = open(root_path + 'sys/class/net/' + i + '/statistics/tx_bytes')
-            r_file = open(root_path + 'sys/class/net/' + i + '/statistics/rx_bytes')
-            s_bytes = int(int(s_file.read().replace('\n', '')) / 1048576)
-            r_bytes = int(int(r_file.read().replace('\n', '')) / 1048576)
-            s_file.close()
-            r_file.close()
-            sent_total += s_bytes
-            received_total += r_bytes
-            net_list.append({'name': i, 'sent': s_bytes, 'received': r_bytes})
-    return {'interfaces': net_list, 'sent_total': sent_total, 'received_total': received_total}
+    try:
+        net_list = []
+        sent_total = 0
+        received_total = 0
+        net_interfaces = listdir(root_path + 'sys/class/net')
+        if 'IGNORE_INTERFACES' in environ:
+            ignored_interfaces = environ['IGNORE_INTERFACES'].split()
+        else:
+            ignored_interfaces = ['lo']
+        for i in net_interfaces:
+            if i not in ignored_interfaces:
+                s_file = open(root_path + 'sys/class/net/' + i + '/statistics/tx_bytes')
+                r_file = open(root_path + 'sys/class/net/' + i + '/statistics/rx_bytes')
+                s_bytes = int(int(s_file.read().replace('\n', '')) / 1048576)
+                r_bytes = int(int(r_file.read().replace('\n', '')) / 1048576)
+                s_file.close()
+                r_file.close()
+                sent_total += s_bytes
+                received_total += r_bytes
+                net_list.append({'name': i, 'sent': s_bytes, 'received': r_bytes})
+        return {'interfaces': net_list, 'sent_total': sent_total, 'received_total': received_total}
+    except FileNotFoundError as e:
+        print('Error when trying to access resources, is the $ROOT_PATH correct?', file=stderr)
+        print(str(e))
 
 
 def diskUsage():
